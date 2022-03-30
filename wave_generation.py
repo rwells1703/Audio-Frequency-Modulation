0f487980cb21ed7fda00c5a78a36da4d59a65f74
Richard Wells <rwells1703@gmail.com>
2022-03-30 19:35:57 +0100

    Added generation of waves with multiple frequencies (for FM).

--- wave_generation.py
+++ wave_generation.py
@@ -5,17 +5,27 @@ from matplotlib import pyplot as plt
 SAMPLE_RATE = 44100
 
 def main():
-    time, amplitude = default_wave(2, 440)
+    time, amplitude = generate_dual_freq_wave(3, 10, 5)
 
     plot_wave(time, amplitude)
-    play_wave(amplitude)
+    #play_wave(amplitude)
 
 # Generates a sine wave for a specified number of seconds
 def generate_wave(seconds, frequency):
-    time = np.linspace(0, seconds, seconds * SAMPLE_RATE, False)
+    time = np.linspace(0, seconds, np.ceil(seconds * SAMPLE_RATE).astype(int), False)
     amplitude = np.sin(frequency * time * 2 * np.pi)
     
     return time, amplitude
+
+# Generates two sine waves of different frequencies and appends them along the time axis
+def generate_dual_freq_wave(seconds, frequency1, frequency2):
+    time1, wave1 = generate_wave(seconds/2, frequency1)
+    time2, wave2 = generate_wave(seconds/2, frequency2)
+    
+    time = np.linspace(0, seconds, np.ceil(seconds * SAMPLE_RATE).astype(int), False)
+    note = np.append(wave1, wave2)
+
+    return time, note
 
 # Plots the note against time, on a graph
 def plot_wave(time, note):
