72903e339751d96ed59aa10f662a626ceb4444a6
Richard Wells <rwells1703@gmail.com>
2022-04-07 14:19:43 +0100

    Fixed problem with ASK algorithm. Also split main wave generation function into ASK and FSK functions.

--- wave_generation.py
+++ wave_generation.py
@@ -2,12 +2,10 @@ import numpy as np
 import simpleaudio
 from matplotlib import pyplot as plt
 
-#megan is the best person ever i love her so much 
-
 SAMPLE_RATE = 44100
 
 def main():
-    time, amplitude = multi_freq_wave([0,1,1,0,1,0,1,1,1,0,0,1,1,0,1], 0.2)
+    time, amplitude = frequency_shift_keying([0,1,1,0,0,0,0,0,1,0,1,1,1,0,0,1,1,1,1])
 
     plot_wave(time, amplitude)
     play_wave(amplitude)
@@ -29,34 +27,37 @@ def generate_dual_freq_wave(seconds, frequency1, frequency2):
 
     return time, note
 
-# Generates an AM or FM wave based upon the input bit array
-def multi_freq_wave(bits, bit_time):
-    modulation = "FM"
-
-    # Frequency values for FM
-    low_freq = 523.25
-    high_freq = 440
-    
-    # Amplitude values for AM
-    low_amp = 0.01
-    high_amp = 1
-    
-    # Default values
-    amplitude = 1
-    freq = 440
-
+# Generates a waveform using Amplitude Shift Keying
+def amplitude_shift_keying(bits, bit_time=0.2, high_amp=1, frequency=440):
     waves = []
+
     for b in bits:
-        if modulation == "AM":
-            if b == 0:
-                amplitude = low_amp
-            else:
-                amplitude = high_amp
-        elif modulation == "FM":
-            if b == 0:
-                freq = low_freq
-            else:
-                freq = high_freq
+        if b == 0:
+            # Generate a flat line for a 0
+            time_divisions = np.ceil(bit_time * SAMPLE_RATE).astype(int)
+            wave = np.full(time_divisions, 0)
+        else:
+            # Generate a wave for a 1
+            time_slice, wave = generate_wave(bit_time, frequency, high_amp)
+
+        waves.append(wave)
+    
+    time = np.linspace(0, bit_time*len(bits), np.ceil(bit_time*len(bits) * SAMPLE_RATE).astype(int), False)
+    note = np.append([], waves)
+
+    return time, note
+
+# Generates a waveform using Frequency Shift Keying
+def frequency_shift_keying(bits, bit_time=0.2, low_freq=440, high_freq=523.25, amplitude=1):
+    waves = []
+
+    for b in bits:
+        if b == 0:
+            # Generate a low frequency wave for a 0
+            freq = low_freq
+        else:
+            # Generate a high frequency wave for a 1
+            freq = high_freq
 
         time_slice, wave = generate_wave(bit_time, freq, amplitude)
         waves.append(wave)
