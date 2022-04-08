60c7a7841e0838f7c5314a818db6ce68533b7d06
Richard Wells <rwells1703@gmail.com>
2022-04-08 02:02:34 +0100

    Added code for converting between data types used during modulation.

---
+++ data_conversion.py
@@ -0,0 +1,33 @@
+# Convert text to a string of 1s and 0s (bit string)
+def text_to_bit_string(text):
+    bit_string = ""
+
+    for t in text:
+        bit_string += format(ord(t), 'b')
+        
+    return bit_string
+
+# Converts a string of bits to an array of integers representing them
+def bits_to_integers(bits, segment_bits):
+    start = 0
+    end = segment_bits
+    
+    # Zero pad the data to a size that is a multiple of the segment bits
+    leftover_bits = len(bits) % segment_bits
+
+    # If there are leftover bits, pad the end
+    if leftover_bits != 0:
+        padding = segment_bits - leftover_bits
+        bits += "0"*(padding)
+
+    integers = []
+
+    # Loop through the bits and convert sections to an integer
+    while end <= len(bits):
+        bit_segment = bits[start:end]
+        integers.append(int(bit_segment, 2))
+        
+        start += segment_bits
+        end += segment_bits
+
+    return integers
