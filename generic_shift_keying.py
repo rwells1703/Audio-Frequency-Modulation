fec2d787381a4a2f50fd2841a489cd42fd28325f
Richard Wells <rwells1703@gmail.com>
2022-04-09 10:53:08 +0100

    Removed deprecated carrier wave dictionary.

--- generic_shift_keying.py
+++ generic_shift_keying.py
@@ -1,8 +1,5 @@
 import utils
 
-# Characteristics of the carrier wave
-carrier_values = {"a":1, "f":5, "p":1}
-
 # Example shift keying (binary shift key modulation on all of: amplitude, frequency and phase)
 #shift_key_possibilites = {"a":[1,0.1], "f":[80,120], "p":[-1,1]}
 shift_key_possibilites = {"a":[1], "f":range(150,9999,20), "p":[1]}
